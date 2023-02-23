a56406c01f300d6ebd100c26eec4851127b689f9
Ariel Alejandro Berurena <100146282+ArielB1991@users.noreply.github.com>
2023-02-23 11:37:06 -0300

    Add files via upload

---
+++ cursos.js
@@ -0,0 +1,44 @@
+let infoCursos= {
+    'programacion':[
+        {
+            id: 1,
+            titulo: 'Aprende Python',
+            lenguaje: 'python',
+            vistas: 15000,
+            nivel: 'basico'
+        },
+        {
+            id:2,
+            titulo:'Python intermedio',
+            lenguaje: 'python',
+            vistas: 13435,
+            nivel: 'intermedio'
+        },
+        {
+            id:3,
+            titulo: 'Aprende Javascript',
+            lenguaje: 'javascript',
+            vistas:13214,
+            nivel: 'basico'
+            
+        }
+    ],
+    'matematicas':[
+        {
+            id: 1,
+            titulo: 'Aprende Calculo',
+            tema: 'calculo',
+            vistas: 11234,
+            nivel: 'basico'
+        },
+        {
+            id: 2,
+            titulo: 'Aprende Algebra',
+            tema: 'algebra',
+            vistas: 12342,
+            nivel: 'intermedio'
+        },
+
+    ]
+}
+module.exports.infoCursos=infoCursos;
